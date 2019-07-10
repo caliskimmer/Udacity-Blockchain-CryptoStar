448fd8480f3b67dacb0b3b10f6683cd8725c63b9
Matas <mempakeris@me.com>
2019-07-09 17:59:15 -0700

    Task 3: Added changes and deployed contract to rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,10 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+// load dependencies
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config()
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -51,6 +55,12 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    rinkeby: {
+        provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+        network_id: 4,
+        gas: 4500000,
+        gasPrice: 10000000000
+    }
 
     // Another network with more advanced options...
     // advanced: {
